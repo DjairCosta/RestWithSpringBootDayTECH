295325bd6668a488344a433be3117a8ea0c7f309
DjairCosta <costadjaircloud@gmail.com>
2021-04-28 01:02:37 -0300

    first commit

---
+++ src/main/java/com/daytech/RestSpringBootDaytech/RestSpringBootDaytechApplication.java
@@ -0,0 +1,13 @@
+package com.daytech.RestSpringBootDaytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootDaytechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootDaytechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daytech/RestSpringBootDaytech/RestSpringBootDaytechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daytech.RestSpringBootDaytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootDaytechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
